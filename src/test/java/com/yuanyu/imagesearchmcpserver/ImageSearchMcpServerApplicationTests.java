783e2a0c8c4b124ee34ea8a2293d2fbeaeb411a5
YUANYU HE <129406409+yuanyu-1016@users.noreply.github.com>
2026-04-08 13:26:42 +0800

    Add files via upload

---
+++ src/test/java/com/yuanyu/imagesearchmcpserver/ImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yuanyu.imagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSearchMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
